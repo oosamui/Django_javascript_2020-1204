cb9493b7fc85d11315ec328000b11a0c7ea20871
oosamui <oosamucvb@gmail.com>
2021-09-19 02:18:14 +0900

    図書館Apiデータを取ってくることはできたが、抽出・検索ができない

---
+++ vuex_chart/pages/coronaTokyo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ title['0'] }}</p>
+    <p>{{ title['1'] }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const url = 'https://api.calil.jp/library?'
+    const apikey = '2ffc7d89470b203a55ea009ccb1cfbfa'
+    const pref = encodeURIComponent('愛知県')
+    // const pref = '&geocode=136.7163027,35.390516&limit=10'
+    const title = await context.$axios.$get(
+      url + 'appkey=' + apikey + '&pref=' + pref + '&format=json'
+    )
+
+    return { title }
+  },
+}
+</script>
