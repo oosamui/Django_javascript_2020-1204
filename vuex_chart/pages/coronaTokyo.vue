61dde54ddb1ec965de1955859d78e971e413bfd8
oosamui <oosamucvb@gmail.com>
2021-09-21 22:10:31 +0900

    P81サイドバーのランキングまでOK

--- vuex_chart/pages/coronaTokyo.vue
+++ vuex_chart/pages/coronaTokyo.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <p>{{ title['0'] }}</p>
-    <p>{{ title['1'] }}</p>
+    <p>{{ titles }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData(context) {
+  asyncData({ $axios }) {
     const url = 'https://api.calil.jp/library?'
     const apikey = '2ffc7d89470b203a55ea009ccb1cfbfa'
     const pref = encodeURIComponent('愛知県')
     // const pref = '&geocode=136.7163027,35.390516&limit=10'
-    const title = await context.$axios.$get(
-      url + 'appkey=' + apikey + '&pref=' + pref + '&format=json'
-    )
-
-    return { title }
+    return $axios
+      .get(url + 'appkey=' + apikey + '&pref=' + pref + '&format=json')
+      .then((res) => {
+        return { titles: res.data }
+      })
   },
 }
 </script>
